9b5109dcd599f60c8873828919217246eea8d5cc
Punkte <paartheepan94@gmail.com>
2019-01-18 09:40:00 +0100

    beginning parallax

--- client/src/Components/EraView/EraView.js
+++ client/src/Components/EraView/EraView.js
@@ -10,18 +10,31 @@ const EraView = (props) => {
   return (
     <Fragment>
 
-      <ParallaxLayer>
-        <EraViewInfo className="EraView__info">
+      <ParallaxLayer offset={props.current - 1} speed={0} style={{cursor: 'pointer'}} onClick={props.onClick} style={{zIndex: '1', backgroundColor: props.section_bgcolor}}>
+        <EraViewInfo className="EraView__info" style={{zIndex: '3'}}>
           <div className="EraView__current" >{props.current} / {props.length}</div>
           <EraViewCurrent className="EraView"/>
-          <h1 className="EraView__title" >{props.name}</h1>
+          <h1 className="EraView__title" dangerouslySetInnerHTML={{ __html: props.name }}>{/*props.name*/}</h1>
           <DashLine />
           <p className="EraView__subTitle">{props.section_subtitle}</p>
         </EraViewInfo>
       </ParallaxLayer>
 
-      <ParallaxLayer>
-        <p>Lol</p>
+      <ParallaxLayer offset={props.current - 1} speed={0.4} onClick={props.onClick} style={{zIndex: '2', 
+          right: '33%',}}>
+        <div
+        style={{
+          backgroundImage: `url(https://via.placeholder.com/500)`,
+          backgroundSize: 'cover',
+          width: '600px',
+          height: '600px',
+          position: 'relative',
+          transform: 'rotate(-5deg)',
+          left: '-50%'
+        }}
+        >
+
+        </div>
       </ParallaxLayer>
 
     </Fragment>
--- client/src/Views/EraSelect/EraSelectContainer.js
+++ client/src/Views/EraSelect/EraSelectContainer.js
@@ -7,19 +7,19 @@ class EraSelectContainer extends Component {
   state = {
     eras: [
       {
-       name: "At the dawn of a new culture", 
+       name: "At the dawn <br/>of a new <br/>culture", 
        section_subtitle: "The begenning of rap and hip-hop. (70s)",
        section_bgcolor: "#F8C918",
        section_color: ""
       },
       {
-        name: "The rise of golden Era",
+        name: "The rise of <br/>golden Era",
         section_subtitle: "The birth of Gangsta rap",
         section_bgcolor: "#B21131",
         section_color: ""
       },
       {
-        name: "The Mumble Rap",
+        name: "The Mumble <br/>Rap",
         section_subtitle: "",
         section_bgcolor: "#561222",
         section_color: ""
@@ -27,19 +27,27 @@ class EraSelectContainer extends Component {
     ],
   }
 
-  scroll = (to) => this.refs.parallax.scrollTo(to)
+  scroll = (current) => {
+    console.log(current)
+    if(current === this.state.eras.length - 1) {
+      this.parallax.scrollTo(0)
+    } else {
+      this.parallax.scrollTo(current+1)
+    }
+  }
 
   render() {
     const {eras} = this.state;
     return (
-      <Parallax className='EraSelectContainer' style={{backgroundColor: "teal"}} pages={eras.length} horizontal scrolling={false}>
+      <Parallax className='EraSelectContainer' ref={ref => this.parallax = ref} style={{backgroundColor: "teal"}} pages={eras.length} horizontal scrolling={false}>
         {eras.map((era, i) => (
           <EraView 
             {...era}
             key={i} 
-            onClick={() => this.scroll(i+1)}
+            onClick={() => this.scroll(i)}
             length={eras.length} 
-            current={i+1}/>
+            current={i+1}
+          />
         ))}
       </Parallax>
     )
