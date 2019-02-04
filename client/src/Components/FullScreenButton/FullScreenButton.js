4970024e7c50e6c26639224857506be9e8300908
maximeoger <maximeoger93@gmail.com>
2019-02-04 09:25:32 +0100

    add fullscreen image

--- client/src/Components/FullScreenButton/FullScreenButton.js
+++ client/src/Components/FullScreenButton/FullScreenButton.js
@@ -2,7 +2,6 @@ import React from 'react';
 import withConsumer from '../../Higher-Order-Components/withConsumer';
 import styled from 'styled-components';
 
-
 const FullScreenButton = styled.button`
   position: fixed;
   bottom: 20px;
