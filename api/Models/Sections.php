cca2a44eef0255b885f753184ba3a0e672a20367
Kevin <kevin.manssat@hotmail.fr>
2019-02-05 14:58:51 +0100

    fix: modified SQL request

--- api/Models/Sections.php
+++ api/Models/Sections.php
@@ -79,130 +79,133 @@ class Sections
   public function getById(int $id) : ? array
   {
     $sql = "SELECT
-    section_id,
-    section_title,
-    section_subtitle,
-    section_bgcolor,
-    section_color,
-    section_bgimage,
-    section_slug
+    sections.section_id,
+    sections.section_subtitle,
+    sections.section_title,
+    sections.section_bgcolor,
+    sections.section_color,
+    sections.section_bgimage,
+    sections.section_slug,
+    content.content_type,
+    content.content_text
     FROM
     sections
+    INNER JOIN content ON sections.section_id = content.fk_section_id
     WHERE
-    section_id = :id;";
+    sections.section_id = :id";
     return $this->executeSqlById($sql, $id);
   }
 
   public function getArtists() : ? array
   {
-    $sql = "SELECT
-    artist_id,
-    artist_name,
-    artist_content,
-    artist_cover,
-    fk_section_id
-    FROM
-    artists;
+    $sql = " SELECT
+      artist_id,
+      artist_name,
+      artist_content,
+      artist_cover,
+      fk_section_id
+      FROM
+      artists;
     ";
     return $this->executeSql($sql);
   }
 
   public function getMusics() : ? array
   {
-    $sql = "SELECT
-    music_id,
-    music_title,
-    music_cover,
-    music_src,
-    music_artist_id,
-    music_content,
-    music_content_color,
-    music_bgcolor,
-    fk_section_id
-    FROM
-    musics
-    ";
+    $sql = " SELECT
+      music_id,
+      music_title,
+      music_cover,
+      music_src,
+      music_artist_id,
+      music_content,
+      music_content_color,
+      music_bgcolor,
+      fk_section_id
+      FROM
+      musics
+      ";
     return $this->executeSql($sql);
   }
 
   public function getContentBySection(int $id) : ? array
   {
-    $sql = "SELECT
-    content_id,
-    content_type,
-    content_text,
-    fk_section_id
-    FROM
-    content
-    WHERE
-    fk_section_id = :id
-    ";
+    $sql = " SELECT
+      content_id,
+      content_type,
+      content_text,
+      fk_section_id
+      FROM
+      content
+      WHERE
+      fk_section_id = : id
+      ";
     return $this->executeSqlById($sql, $id);
   }
 
   public function getContentByType(int $id, string $act) : ? array
   {
-    $sql = "SELECT
-    content_id,
-    content_type,
-    content_text,
-    fk_section_id
-    FROM
-    content
-    WHERE
-    fk_section_id = :id
-    AND
-    content_type = :act
-    ";
+    $sql = " SELECT
+      content_id,
+      content_type,
+      content_text,
+      fk_section_id
+      FROM
+      content
+      WHERE
+      fk_section_id = : id
+      and
+      content_type = : act
+      ";
     return $this->executeSqlByIdAndAction($sql, $id, $act);
   }
 
   public function getArtistBySection(int $id) : ? array
   {
-    $sql = "SELECT
-    artist_id,
-    artist_name,
-    artist_content,
-    artist_cover,
-    fk_section_id
-    FROM
-    artists
-    WHERE
-    fk_section_id = :id
-    ";
+    $sql = " SELECT
+      artist_id,
+      artist_name,
+      artist_content,
+      artist_cover,
+      fk_section_id
+      FROM
+      artists
+      WHERE
+      fk_section_id = : id
+      ";
     return $this->executeSqlById($sql, $id);
   }
 
   public function getMusicBySection(int $id) : ? array
   {
-    $sql = "SELECT
-    music_id,
-    music_title,
-    music_cover,
-    music_src,
-    music_artist_id,
-    music_content,
-    music_content_color,
-    music_bgcolor,
-    fk_section_id
-    FROM
-    musics
-    WHERE
-    fk_section_id = :id
-    ";
+    $sql = " SELECT
+      music_id,
+      music_title,
+      music_cover,
+      music_src,
+      music_artist_id,
+      music_content,
+      music_content_color,
+      music_bgcolor,
+      fk_section_id
+      FROM
+      musics
+      WHERE
+      fk_section_id = : id
+      ";
     return $this->executeSqlById($sql, $id);
   }
   public function getPlaylistBySection(int $id) : ? array
   {
-    $sql = "SELECT
-    playlist_id,
-    playlist_spotify_id,
-    fk_section_id
-    FROM
-    playlists
-    WHERE
-    fk_section_id = :id
-    ";
+    $sql = " SELECT
+      playlist_id,
+      playlist_spotify_id,
+      fk_section_id
+      FROM
+      playlists
+      WHERE
+      fk_section_id = : id
+      ";
     return $this->executeSqlById($sql, $id);
   }
 
