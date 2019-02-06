9f499d483046acf47bc1e87ade28cf6b9e51bc90
Punkte <paartheepan94@gmail.com>
2019-02-07 00:25:38 +0100

    feature: styled components in hall of fame 💅

--- client/src/Components/StyledComponents/HallOfFame.js
+++ client/src/Components/StyledComponents/HallOfFame.js
@@ -34,7 +34,6 @@ export const HallContainer = styled.section `
 export const ArtistCard = styled.article `
 
     min-width: 100px;
-    max-width: 250px;
     padding: 0 25px;
 
     color: #fff;
@@ -43,9 +42,12 @@ export const ArtistCard = styled.article `
     font-size: 14px;
     font-weight: 600;
     
+    transform: ${ props => props.unlocked ? 'scale(1.05)' : '' };
+
+    cursor: ${ props => props.unlocked ? 'pointer' : 'default' };
 `
 
-export const ArticleCardImage = styled.img`
+export const ArtistCardImage = styled.img`
 
     width: 100%;
     max-width: 250px;
@@ -54,10 +56,100 @@ export const ArticleCardImage = styled.img`
     
 `
 
-export const ArticleCardName = styled.p`
+export const ArtistCardName = styled.p`
 
     display: ${ props => props.unlocked === true ? 'block' : 'none'};
     margin-bottom: 32px;
     text-align: center;
+
+`
+
+export const ModalOverlay = styled.div`
+
+    overflow: hidden;
+
+`
+
+export const ModalContainer = styled.div`
+
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin: 0 auto;
+    margin-top: 80px;
+    height: calc(100vh - 80px);
+    width: 90%;
+    max-width: 800px;
+
+`
+
+export const ModalCloser = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 42px;
+    height: 42px;
+    margin: 24px auto;
+
+    background-color: #F8C918;
+    border-radius: 100%;
+    color: #1C1C1C;
+
+    font-family: 'Montserrat', Arial, Helvetica, sans-serif;
+    font-weight: 900;
+    line-height: 15px;
+    font-size: 11px;
+
+    cursor: pointer;
+`
+
+export const ModalCard = styled.article`
+    
+    margin-bottom: 24px;
+
+    color: #000;
+    font-family: 'Montserrat', Arial, Helvetica, sans-serif;
+    line-height: 16px;
+    font-size: 14px;
+    font-weight: 600;
+
+`
+
+export const ModalCardImage = styled.img`
+
+    width: 100%;
+    max-width: 250px;
+
+    filter: ${ props => props.unlocked === true ? 'none' : 'grayscale(100%)'};
+    
+`
+
+export const ModalTitle = styled.h2`
+
+    font-family: 'Montserrat', Arial, Helvetica, sans-serif;
+    font-style: normal;
+    font-weight: 900;
+    line-height: 15px;
+    font-size: 28px;
+    text-transform: uppercase;
+
+    color: #1C1C1C;
+
+`
+
+export const ModalContent = styled.div`
+
+    margin-top: 74px;
+    height: 100%;
+
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
+    font-style: italic;
+    font-weight: 500;
+    line-height: 36px;
+    font-size: 24px;
+
+    color: #1C1C1C;
+
+    overflow: scroll;
 
 `
