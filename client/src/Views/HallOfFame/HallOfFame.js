c69f44f7d1f9c0d67b9461788d2017db1b02b6fe
Punkte <paartheepan94@gmail.com>
2019-02-06 16:39:06 +0100

    feature: display or not unlocked artist in hof

--- client/src/Views/HallOfFame/HallOfFame.js
+++ client/src/Views/HallOfFame/HallOfFame.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import withConsumer from '../../Higher-Order-Components/withConsumer';
 import rapApi from '../../helpers/api'
+import { Link } from 'react-router-dom'
 
 import {
   Main,
   Title,
   HallContainer,
-  ArtistCard
+  ArtistCard,
+  ArticleCardImage,
+  ArticleCardName
 } from '../../Components/StyledComponents/HallOfFame'
+
 
 class HallOfFame extends Component {
 
@@ -15,6 +20,8 @@ class HallOfFame extends Component {
   }
 
   async componentDidMount() {
+    const { context } = this.props
+    context.setAmbientMusic('http://54.190.49.146/musics/what_so_not.mp3')
     const artists = await rapApi.getArtists()
     console.log(artists)
     this.setState({ artists })
@@ -22,6 +29,7 @@ class HallOfFame extends Component {
   }
   render(){
     const { artists } = this.state
+    const { isUnlockedArtist } = this.props.context
     if (artists === null) return <div>Loading</div>
     return artists && (
       <Main>
@@ -29,14 +37,19 @@ class HallOfFame extends Component {
         <Title>The Hall Of Fame</Title>
         <HallContainer>
           {
-            artists.map( artist => (
-              <ArtistCard key={ artist.artist_id } >
-                <img src={ artist.artist_cover }/>
-                <p style={ { textAlign: 'center'} }>
-                  { artist.artist_name }<br/>
-                </p>
-              </ArtistCard>
-            ) )
+            artists.map( artist => {
+              const unlocked = isUnlockedArtist(artist.artist_id)
+              return (
+              <Link key={ artist.artist_id } to={`/g/artist/${artist.artist_id}`} style={{ textDecoration: 'none' }}>
+                <ArtistCard>
+                  <ArticleCardImage src={ /* artist.artist_cover */ `https://image.noelshack.com/fichiers/2019/06/3/1549451020-group-7.png` } unlocked={unlocked} />
+                  <ArticleCardName style={ { textAlign: 'center'} } unlocked={unlocked} >
+                    { artist.artist_name }
+                  </ArticleCardName>
+                </ArtistCard>
+              </Link>
+            ) }
+            )
           }
         </HallContainer>
         
@@ -45,4 +58,4 @@ class HallOfFame extends Component {
   }
 }
 
-export default HallOfFame;
+export default withConsumer(HallOfFame);
