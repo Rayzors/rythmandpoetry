624aef3153a121c931bdef03b718fd593888520d
Kevin <kevin.manssat@hotmail.fr>
2019-02-07 12:42:54 +0100

    change wallOfFame img && css rules

--- client/src/Components/StyledComponents/SectionCover.js
+++ client/src/Components/StyledComponents/SectionCover.js
@@ -7,6 +7,8 @@ const SectionCover = styled.section`
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
+  background-size: cover;
+  background-repeat: no-repeat;
   background-color: ${(props) => props.backgroundColor || 'none'};
   background-image: ${(props) => `url(${props.backgroundImage})` || 'none'};
   position: relative;
